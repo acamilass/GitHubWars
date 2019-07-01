eabd85c94887cb62c15e115eae05e139bc372b52
Caio Santos <caio.santos687@gmail.com>
2019-07-01 11:30:55 -0300

    finish result page

--- js/batalha.js
+++ js/batalha.js
@@ -19,15 +19,15 @@ function Usuario(nome, imagem, repositorio, seguidores, seguindo, gists) {
     this.gists = gists;
     this.GetTotalPontos = () => {
         let pontuacaoRepositorio = this.repositorio * 20;
-        let pontuacaoFollowers = this.seguidores * 10;
+        let pontuacaoSeguidores = this.seguidores * 10;
         let pontuacaoSeguindo = this.seguindo * 5;        
         let pontuacaoGist = this.gists * 5;
 
-        let totalDePontos = pontuacaoRepositorio + pontuacaoFollowers + pontuacaoSeguindo + pontuacaoGist;
+        let totalDePontos = pontuacaoRepositorio + pontuacaoSeguidores + pontuacaoSeguindo + pontuacaoGist;
 
         return {
             pontuacaoRepositorio,
-            pontuacaoFollowers,
+            pontuacaoSeguidores,
             pontuacaoSeguindo,            
             pontuacaoGist,
             totalDePontos
@@ -48,10 +48,12 @@ botaoLutar.addEventListener('click', function () {
     getUser(url1, function (json) {
         user1 = new Usuario(json.name, json.avatar_url, json.public_repos,
             json.followers, json.following, json.public_gists);
+        user1.pontos = user1.GetTotalPontos();
 
         getUser(url2, function (json) {
             user2 = new Usuario(json.name, json.avatar_url, json.public_repos,
                 json.followers, json.following, json.public_gists);
+            user2.pontos = user2.GetTotalPontos();
 
             localStorage.setItem('user1', JSON.stringify(user1));
             localStorage.setItem('user2', JSON.stringify(user2));
--- js/resultado.js
+++ js/resultado.js
@@ -1,22 +1,41 @@
-let usuarioVencedor = JSON.parse(localStorage.getItem('vencedor'));
 let user1 = JSON.parse(localStorage.getItem('user1'));
 let user2 = JSON.parse(localStorage.getItem('user2'));
 
-let pontuacaoUser1 = user1.GetTotalPontos();
+//Preenche todos os campos do usuário1
+document.getElementById('img_user1').setAttribute('src', user1.imagem);
 document.getElementById('qtdRepo_user1').innerText =   user1.repositorio;
-document.getElementById('qtdRepoPontos_user1').innerText = pontuacaoUser1.pontuacaoRepositorio;
+document.getElementById('qtdRepoPontos_user1').innerText = user1.pontos.pontuacaoRepositorio;
+document.getElementById('qtdFlw_user1').innerText = user1.seguidores;
+document.getElementById('qtdFlwPontos_user1').innerText = user1.pontos.pontuacaoSeguidores;
+document.getElementById('qtdFlwg_user1').innerText = user1.seguindo;
+document.getElementById('qtdFlwgPontos_user1').innerText = user1.pontos.pontuacaoSeguidores;
+document.getElementById('qtdStars_user1').innerText = 0;
+document.getElementById('qtdStarsPontos_user1').innerText = 0;
+document.getElementById('qtdGists_user1').innerText = user1.gists;
+document.getElementById('qtdGistsPontos_user1').innerText = user1.pontos.pontuacaoGist
+document.getElementById('qtdPontos_user1').innerText = user1.pontos.totalDePontos;
 
-                        // <td id="qtdFlw_user1"></td>
-                        // <td id="qtdFlwPontos_user1"></td>
+//Preenche todos os campos do usuário1
+document.getElementById('img_user2').setAttribute('src', user2.imagem);
+document.getElementById('qtdRepo_user2').innerText =   user2.repositorio;
+document.getElementById('qtdRepoPontos_user2').innerText = user2.pontos.pontuacaoRepositorio;
+document.getElementById('qtdFlw_user2').innerText = user2.seguidores;
+document.getElementById('qtdFlwPontos_user2').innerText = user2.pontos.pontuacaoSeguidores;
+document.getElementById('qtdFlwg_user2').innerText = user2.seguindo;
+document.getElementById('qtdFlwgPontos_user2').innerText = user2.pontos.pontuacaoSeguindo;
+document.getElementById('qtdStars_user2').innerText = 0;
+document.getElementById('qtdStarsPontos_user2').innerText = 0;
+document.getElementById('qtdGists_user2').innerText = user2.gists;
+document.getElementById('qtdGistsPontos_user2').innerText = user2.pontos.pontuacaoGist;
+document.getElementById('qtdPontos_user2').innerText = user2.pontos.totalDePontos;
 
-                        // <td id="qtdFlwg_user1"></td>
-                        // <td id="qtdFlwgPontos_user1"></td>
 
-                        // <td id="qtdStars_user1"></td>
-                        // <td id="qtdStarsPontos_user1"></td>
-
-                        // <td id="qtdGists_user1"></td>
-                        // <td id="qtdGistsPontos_user1"></td>
-
-
-
+//Verificar Vencedor
+let trofeu;
+if(user1.pontos.totalDePontos > user2.pontos.totalDePontos){
+  trofeu = document.getElementById('trophy_user1');
+}
+else {
+  trofeu = document.getElementById('trophy_user2');
+}
+trofeu.style.display = 'block';
